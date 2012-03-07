009969e125f696a8117504a0d76e55cc0f416f09
kalail <kashif610@gmail.com>
2012-03-07 03:55:04 -0600

    Write some initial unit test
    -Add 404 error page

--- kalail/blog/tests.py
+++ kalail/blog/tests.py
@@ -12,5 +12,11 @@ class blog_views_testcase(TestCase):
 		self.assertEqual(response.context['all_posts'].get(id=2).title , 'Second Post!')
 
 	def test_show_post(self):
-		response = self.client.get('/post')
+		response = self.client.get('/post/')
 		self.assertEqual(response.status_code, 404)
+
+	def test_add_comment(self):
+		post = Post.objects.get(pk=1)
+		response = self.client.post('/1/add_comment/', {'new_comment_text': 'yo', 'new_comment_author': 'troll', 'new_comment_captcha': 'red' })
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], 'http://testserver/1/')
