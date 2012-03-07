7d30700984eb4320a510101f5da1d7f13cf29225
kalail <kashif610@gmail.com>
2012-03-07 03:34:23 -0600

    Add test framework
    -Add fixtures

--- kalail/blog/tests.py
+++ kalail/blog/tests.py
@@ -1,16 +1,16 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from blog.models import Post, Comment
+import datetime
 
+class blog_views_testcase(TestCase):
+	fixtures = ['blog_views_testdata.json']
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_index(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTrue('all_posts' in response.context)
+		self.assertEqual(response.context['all_posts'].get(id=2).title , 'Second Post!')
+
+	def test_show_post(self):
+		response = self.client.get('/post')
+		self.assertEqual(response.status_code, 404)
