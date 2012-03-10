61629fe24452fb65ef0c65b76efbe136d20a35d5
kalail <kashif610@gmail.com>
2012-03-10 01:40:00 -0600

    Add more blog tests.

--- kalail/blog/tests.py
+++ kalail/blog/tests.py
@@ -1,22 +1,37 @@
 from django.test import TestCase
 from blog.models import Post, Comment
+from django.core.urlresolvers import reverse
 import datetime
 
-class blog_views_testcase(TestCase):
+class views_testcase(TestCase):
 	fixtures = ['blog_views_testdata.json']
 
-	def test_index(self):
-		response = self.client.get('/')
+	def test_index_success(self):
+		response = self.client.get(reverse('blog.views.index'))
 		self.assertEqual(response.status_code, 200)
-		self.assertTrue('all_posts' in response.context)
-		self.assertEqual(response.context['all_posts'].get(id=2).title , 'Second Post!')
+	
+	def test_show_post_success(self):
+		response = self.client.get(reverse('blog.views.show_post', args=('1')))
+		self.assertEqual(response.status_code, 200)
 
-	def test_show_post(self):
-		response = self.client.get('/post/')
+	def test_show_post_fail(self):
+		response = self.client.get('/-1/')
 		self.assertEqual(response.status_code, 404)
 
-	def test_add_comment(self):
-		post = Post.objects.get(pk=1)
-		response = self.client.post('/1/add_comment/', {'new_comment_text': 'yo', 'new_comment_author': 'troll', 'new_comment_captcha': 'red' })
+	def test_add_comment_success(self):
+		test_comment = Comment(text='This is a test!', author='Tester')
+		response = self.client.post('/1/add_comment/', {'new_comment_text': test_comment.text, 'new_comment_author': test_comment.author, 'new_comment_captcha': 'red' })
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], 'http://testserver/1/')
+		post = Post.objects.get(pk=1)
+		self.assertEqual(post.comment_set.latest('created_on').text, test_comment.text)
+		self.assertEqual(post.comment_set.latest('created_on').author, test_comment.author)
+
+	def test_add_comment_fail(self):
+		test_comment = Comment(text='This is a test!', author='Tester')
+		response = self.client.post('/1/add_comment/', {'new_comment_text': test_comment.text, 'new_comment_author': test_comment.author, 'new_comment_captcha': 'blue' })
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], 'http://testserver/1/')
+		post = Post.objects.get(pk=1)
+		self.assertNotEqual(post.comment_set.latest('created_on').text, test_comment.text)
+		self.assertNotEqual(post.comment_set.latest('created_on').author, test_comment.author)
