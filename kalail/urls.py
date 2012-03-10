6ddad2315fd77e37eabb634adc331477cebfde4a
kalail <kashif610@gmail.com>
2012-03-10 04:33:54 -0600

    Make home url redirect to blog.

--- kalail/urls.py
+++ kalail/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', redirect_to, {'url': '/blog/'}),
 )
