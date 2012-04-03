5ea5b053513366f91915b03429122c84393ab6ee
kalail <kashif610@gmail.com>
2012-04-03 10:36:56 -0500

    Fix: Correctly separate 'Versioning' app.

--- kalail/urls.py
+++ kalail/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^blog/', include('blog.urls')),
-	url(r'^versions/', 'versioning.views.index'),
+	url(r'^versions/', include('versioning.urls')),
     # Examples:
     # url(r'^$', 'kalail.views.home', name='home'),
     # url(r'^kalail/', include('kalail.foo.urls')),
