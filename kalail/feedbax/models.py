7193de13f52c7a1954a788ce6e2b0add5b1f3b36
Kalail <kashif610@gmail.com>
2012-04-14 15:41:00 -0500

    Enhance: Add Feedback model skeleton.

--- kalail/feedbax/models.py
+++ kalail/feedbax/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+	content = models.TextField(max_length=64)
+	user = models.CharField()
+	system = 
+	created_on = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.content
+
+class TestSystem(models.Model):
+	name = models.TextField()
+	site = models.CharField(max_length=32)
+	feedbax_site
+	email = models.ForeignKey(Post)
+	created_on = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.name
