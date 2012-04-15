78d452378085996769165fb88b20eb0a44176e36
Kalail <kashif610@gmail.com>
2012-04-15 18:21:49 -0500

    Feature: Add Feedbax email functionality

--- kalail/feedbax/views.py
+++ kalail/feedbax/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 from django.template.defaultfilters import slugify
+from django.core.mail import EmailMessage
 
 @login_required
 def index(request):
@@ -27,7 +28,14 @@ def setup_feedback_system(request):
 		new_system.save()
 		new_system.users.add(new_user)
 
+		email_message = "Thank you for setting up your " + new_name + " Feedbax.\n\
+		\n\
+		Send in feedback to " + request.build_absolute_uri(reverse('feedbax.views.send_feedback', args=[new_feedbax_site])) + "\n\
+		Feedback will automatically forwarded to " + new_report_email
+
 		# Send email
+		setup_email = EmailMessage('Setup Complete', email_message, to=[new_report_email, request.user.email])
+		setup_email.send()
 		
 		return HttpResponseRedirect(reverse('feedbax.views.index'))
 	return render_to_response('feedbax/setup_feedback_system.html', {}, context_instance=RequestContext(request))
@@ -44,12 +52,26 @@ def send_feedback(request, feedbax_site):
 	if request.method == 'POST':
 		new_content = request.POST['new_feedback_content']
 		new_user = request.user
-		new_system = system
 
-		new_feedback = Feedback(content=new_content, system=new_system, user=new_user)
+		new_feedback = Feedback(content=new_content, system=system, user=new_user)
 		new_feedback.save()
 		
 		# Send email
+		email_message = "Feedbax report: Feedback recieved for " + system.name + "\n\
+		\n\
+		\"" + new_content + "\"\n\
+		By: " + new_user.email + "\n\
+		\n\
+		Send in feedback to " + request.build_absolute_uri(reverse('feedbax.views.send_feedback', args=[system.feedbax_site])) + "\n\
+		Feedback will automatically forwarded to " + system.report_email
+
+		# Send email
+		feedback_email = EmailMessage('Feedbax report: ' + system.name, email_message, to=[system.report_email])
+		feedback_email.send()
+
+		# Add user to feedback system
+		if not new_user in system.users.all():
+			system.users.add(new_user)
 
 		return HttpResponseRedirect(reverse('feedbax.views.index'))
 	return render_to_response('feedbax/send_feedback.html', {'system': system}, context_instance=RequestContext(request))
