91633e77ae8c7f024ebbdd11fbca6e440aefbc30
kalail <kashif610@gmail.com>
2012-04-24 01:51:01 -0500

    Feature: Add sign out functionality

--- kalail/main/urls.py
+++ kalail/main/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('main.views',
 	url(r'^$', 'index'),
 	url(r'^sign_in_needed/$', 'sign_in_needed'),
 	url(r'^sign_in/$', 'sign_in'),
+	url(r'^sign_out/$', 'sign_out'),
 	url(r'^about/$', 'about'),
 	url(r'^contact/$', 'contact'),
 	)
--- kalail/main/views.py
+++ kalail/main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from django.contrib.auth import logout
 
 
 def index(request):
@@ -22,3 +23,8 @@ def about(request):
 
 def contact(request):
 	return render_to_response('main/contact.html', context_instance=RequestContext(request))
+
+def sign_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('main.views.index'))
+
