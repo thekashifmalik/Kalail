d043e098dff04f2487ea7d0efe24f957c1b4fcd6
Kalail <kashif610@gmail.com>
2012-05-08 16:54:22 -0500

    Feature: Add tests for Notes app.

--- kalail/notes/tests.py
+++ kalail/notes/tests.py
@@ -1,16 +1,34 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from notes.models import Notepad
+from django.core.urlresolvers import reverse
+import datetime
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class notes_testcase(TestCase):
+
+	def setUp(self):
+		User.objects.create_user('test_user_name', 'test_user@email.com', 'test_user_password')
+
+	def test_signed_out_index_response_success(self):
+		response = self.client.get(reverse('notes.views.index'))
+		self.assertEqual(response.status_code, 302)
+
+	def test_signed_in_index_response_success(self):
+		self.client.login(username='test_user_name', password='test_user_password')
+		response = self.client.get(reverse('notes.views.index'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_signed_in_index_creates_notepad_success(self):
+		self.client.login(username='test_user_name', password='test_user_password')
+		response = self.client.get(reverse('notes.views.index'))
+		user = User.objects.get(id = self.client.session['_auth_user_id'])
+		self.assertIsInstance(user.notepad, Notepad)
+
+	def test_signed_in_index_shows_notepad_success(self):
+		self.client.login(username='test_user_name', password='test_user_password')
+		user = User.objects.get(id = self.client.session['_auth_user_id'])
+		user.notepad = Notepad(body = "Test Test Test")
+		user.notepad.save()
+		response = self.client.get(reverse('notes.views.index'))
+		self.assertContains(response, user.notepad.body)
