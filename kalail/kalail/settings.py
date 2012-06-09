876bb9104607b928bad62908c2665c1a6c737ece
kalail <kashif610@gmail.com>
2012-06-08 23:31:40 -0500

    Enhance: Comment out Heroku database settings.

--- kalail/kalail/settings.py
+++ kalail/kalail/settings.py
@@ -257,43 +257,43 @@ else:
     CELERY_REDIS_DB = 0
 
 
-# Heroku Database settings
+# # Heroku Database settings
 
-import sys
-import urlparse
-import os
+# import sys
+# import urlparse
+# import os
 
 
-# Register database schemes in URLs.
-urlparse.uses_netloc.append('postgres')
-urlparse.uses_netloc.append('mysql')
+# # Register database schemes in URLs.
+# urlparse.uses_netloc.append('postgres')
+# urlparse.uses_netloc.append('mysql')
 
-try:
+# try:
 
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
+#     # Check to make sure DATABASES is set in settings.py file.
+#     # If not default to {}
 
-    if 'DATABASES' not in locals():
-        DATABASES = {}
+#     if 'DATABASES' not in locals():
+#         DATABASES = {}
 
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+#     if 'DATABASE_URL' in os.environ:
+#         url = urlparse.urlparse(os.environ['DATABASE_URL'])
 
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
+#         # Ensure default database exists.
+#         DATABASES['default'] = DATABASES.get('default', {})
 
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-        if url.scheme == 'postgres':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+#         # Update with environment configuration.
+#         DATABASES['default'].update({
+#             'NAME': url.path[1:],
+#             'USER': url.username,
+#             'PASSWORD': url.password,
+#             'HOST': url.hostname,
+#             'PORT': url.port,
+#         })
+#         if url.scheme == 'postgres':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
+#         if url.scheme == 'mysql':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+# except Exception:
+#     print 'Unexpected error:', sys.exc_info()
