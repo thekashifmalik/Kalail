328f72ccf1dbbc5eec4cc6d4e1b539177b8f785a
kalail <kashif610@gmail.com>
2012-06-10 19:54:08 -0500

    Enhance: Add debug case to Email Send

--- blog/ajax.py
+++ blog/ajax.py
@@ -2,6 +2,7 @@ from django.core import serializers
 from dajaxice.decorators import dajaxice_register
 from django.utils import simplejson
 from blog.models import Post, Comment
+from kalail.settings import DEBUG
 
 @dajaxice_register
 def add_comment(request, post_id, new_comment):
@@ -16,6 +17,7 @@ def add_comment(request, post_id, new_comment):
 			new_comment_post = Post.objects.get(id=post_id)
 			new_comment = Comment(post=new_comment_post, text=new_comment_text, author=new_comment_author)
 			new_comment.save()
-			#send_comment_email.delay()
+			if not DEBUG:
+				send_comment_email.delay()
 	return simplejson.dumps({'text':new_comment.text, 'author':new_comment.author, 'created_on':new_comment.created_on.strftime("%m/%d/%Y %I:%M %p")})
 	#return HttpResponseRedirect(reverse('blog.views.show_post', args=(post_id)))
