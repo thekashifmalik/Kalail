098f43c966741e2d0f1d51243341803e23fb58ba
kalail <kashif610@gmail.com>
2012-06-15 11:27:03 -0500

    Enhance: Add retrieve task id ability

--- blog/ajax.py
+++ blog/ajax.py
@@ -19,6 +19,6 @@ def add_comment(request, post_id, new_comment):
 			new_comment = Comment(post=new_comment_post, text=new_comment_text, author=new_comment_author)
 			new_comment.save()
 			if not DEBUG:
-				send_comment_email.delay()
+				task_id = send_comment_email.delay().task_id
 	return simplejson.dumps({'text':new_comment.text, 'author':new_comment.author, 'created_on':new_comment.created_on.strftime("%m/%d/%Y %I:%M %p")})
 	#return HttpResponseRedirect(reverse('blog.views.show_post', args=(post_id)))
