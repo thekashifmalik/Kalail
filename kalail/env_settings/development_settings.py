865a27d1edcef014a2bebc3ed3af33cbeb1b6dc9
kalail <kashif610@gmail.com>
2012-08-13 07:19:56 +0500

    Remove passwords from VCS

---
+++ kalail/env_settings/development_settings.py
@@ -0,0 +1,49 @@
+# Django settings for kalail project.
+import socket
+import os
+import django
+
+DEBUG = TEMPLATE_DEBUG = False
+STATIC_URL = 'https://s3.amazonaws.com/kalail_static/'
+
+# calculated paths for django and the site
+# used as starting points for various other paths
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__) + "/../../")
+
+def relative_path(relative_root_path, target_file):
+    return os.path.join(relative_root_path, target_file)
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+# Redis
+import os
+import urlparse
+
+redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost'))
+BROKER_URL = os.environ.get('REDISTOGO_URL')
+CELERY_REDIS_HOST = redis_url.hostname
+CELERY_REDIS_PORT = redis_url.port
+CELERY_REDIS_DB = 0
+
+# Not Working with Django ORM
+# CELERYBEAT_SCHEDULER = " djcelery.schedulers.DatabaseScheduler"
+
+# Dajax
+DAJAXICE_MEDIA_PREFIX ="dajaxice"
+DAJAXICE_DEBUG = False
+
+# Protected variables
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Amazon S3 setting
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
--- kalail/settings.py
+++ kalail/settings.py
@@ -3,22 +3,14 @@ import socket
 import os
 import django
 
-# app_env = os.environ["KALAIL_ENV"]
+app_env = os.environ["KALAIL_ENV"]
 
-# if app_env == "production":
-
-# else if app_env == "development":
-
-# else:
-
-
-if socket.gethostname() == 'Kalail-PC':
-    DEBUG = TEMPLATE_DEBUG = False
-    STATIC_URL = 'https://s3.amazonaws.com/kalail_static/'
+if app_env == "production":
+    from kalail.env_settings.production_settings import *
+elif app_env == "development":
+    from kalail.env_settings.development_settings import *
 else:
-    DEBUG = TEMPLATE_DEBUG = False
-    STATIC_URL = 'https://s3.amazonaws.com/kalail_static/'
-
+    from kalail.env_settings.local_settings import *
 
 # calculated paths for django and the site
 # used as starting points for various other paths
@@ -33,21 +25,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-if socket.gethostname() == 'Kalail-PC':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'kalail_db',                      # Or path to database file if using sqlite3.
-            'USER': 'kalail_db_user',                      # Not used with sqlite3.
-            'PASSWORD': 'kalail_db_password',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-    }
-else:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -88,10 +65,6 @@ MEDIA_URL = ''
 #STATIC_ROOT = relative_path(SITE_ROOT, 'static')
 STATIC_ROOT = '/static/'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-
-
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
@@ -113,8 +86,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'dhejs&r+c7l!!(7r79zd_je1&e549&w0y72f$+ljj*n107#xew'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -191,21 +162,9 @@ LOGGING = {
     }
 }
 
-# Amazon S3 setting
 STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = 'AKIAI7EOYMFRURZPCUKA'
-AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'kalail_static'
 AWS_TEMP_STORAGE_BUCKET_NAME = 'kalail_temp'
-
-# Django debug toolbar
-INTERNAL_IPS = (
-    '127.0.0.1',
-    )
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 
 # Set up Cache
 CACHES = {
@@ -218,12 +177,10 @@ CACHES = {
 # Set up Sessions
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
-
 # Email Setup
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kalail@kalail.com'
-EMAIL_HOST_PASSWORD = 'batmanbatman'
 EMAIL_PORT = 587
 
 # Google Authentication
@@ -255,30 +212,3 @@ TWITTER_CACHE_TIMEOUT = 60 * 5
 # Celery settings
 import djcelery
 djcelery.setup_loader()
-
-# Redis
-import os
-import urlparse
-
-CELERY_RESULT_BACKEND = "redis"
-if socket.gethostname() == 'Kalail-PC':
-    BROKER_URL = "redis://localhost:6379/0"
-    CELERY_REDIS_HOST = "localhost"
-    CELERY_REDIS_PORT = 6379
-    CELERY_REDIS_DB = 0
-else:
-    redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost'))
-    BROKER_URL = os.environ.get('REDISTOGO_URL')
-    CELERY_REDIS_HOST = redis_url.hostname
-    CELERY_REDIS_PORT = redis_url.port
-    CELERY_REDIS_DB = 0
-
-# Not Working with Django ORM
-# CELERYBEAT_SCHEDULER = " djcelery.schedulers.DatabaseScheduler"
-
-# Dajax
-DAJAXICE_MEDIA_PREFIX ="dajaxice"
-if socket.gethostname() == 'Kalail-PC':
-    DAJAXICE_DEBUG = True
-else:
-    DAJAXICE_DEBUG = False
