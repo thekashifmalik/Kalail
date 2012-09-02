85fef4860dd3051107d0f2aa6a3066e943f877ca
kalail <kashif610@gmail.com>
2012-09-03 00:08:14 +0500

    Remove Feedbax app files.

--- feedbax/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('feedbax.views',
-	url(r'^$', 'index'),
-	url(r'^setup/$', 'setup_feedback_system'),
-	url(r'^show/(?P<feedbax_site>.+)/$', 'show_system_feedback'),
-	url(r'^(?P<feedbax_site>.+)/$', 'send_feedback'),
-	)
