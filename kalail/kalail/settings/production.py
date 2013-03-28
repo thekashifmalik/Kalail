29101deee019b45880cb275164b9d2e0794aab3a
Kalail <kalail@ubuntu.(none)>
2013-03-28 05:49:21 -0500

    Set production settings

--- kalail/kalail/settings/production.py
+++ kalail/kalail/settings/production.py
@@ -1,58 +1,36 @@
-# Django settings for kalail project.
-import socket
+from .base import *
 import os
-import django
 
-DEBUG = TEMPLATE_DEBUG = False
-STATIC_URL = 'https://s3.amazonaws.com/kalail_static/'
+# Debug
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
-# calculated paths for django and the site
-# used as starting points for various other paths
-DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__) + "/../../")
-
-def relative_path(relative_root_path, target_file):
-    return os.path.join(relative_root_path, target_file)
-
+# Database
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-# Redis
-import os
-import urlparse
-
-redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost'))
-BROKER_URL = os.environ.get('REDISTOGO_URL')
-CELERY_REDIS_HOST = redis_url.hostname
-CELERY_REDIS_PORT = redis_url.port
-CELERY_REDIS_DB = 0
-
-# Not Working with Django ORM
-# CELERYBEAT_SCHEDULER = " djcelery.schedulers.DatabaseScheduler"
-
-# Dajax
-DAJAXICE_MEDIA_PREFIX ="dajaxice"
-DAJAXICE_DEBUG = False
-
-# Protected variables
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Amazon S3 setting
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'kalail'
 
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FILE_STORAGE = 'helpers.storages.MediaS3Storage'
+STATICFILES_STORAGE = 'helpers.storages.StaticS3Storage'
 
+# URL prefix for files.
+STATIC_URL = 'https://s3.amazonaws.com/' + AWS_STORAGE_BUCKET_NAME + '/static/'
+MEDIA_URL = 'https://s3.amazonaws.com/' + AWS_STORAGE_BUCKET_NAME + '/media/'
+
+# RabbitMQ
+BROKER_URL = os.environ.get('CLOUDAMQP_URL')
+BROKER_POOL_LIMIT = 1
+
+# Memcachier
 os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '')
 os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
 os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
 
-# Set up Cache
+# Cache settings
 CACHES = {
  	'default': {
 		'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
@@ -61,6 +39,13 @@ CACHES = {
 		'BINARY': True,
 	}
 }
+
+ALLOWED_HOSTS = [
+	'www.kalail.com',
+	's3.amazonaws.com'
+]
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Browser ID settings
 SITE_URL = 'http://www.kalail.com'
