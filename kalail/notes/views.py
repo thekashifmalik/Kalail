6e71f818c124029feabccfb4f8e32ad1ddebd30e
Kalail <kalail@ubuntu.(none)>
2013-03-28 05:33:26 -0500

    Update notepad views.

--- kalail/notes/views.py
+++ kalail/notes/views.py
@@ -1,26 +1,28 @@
 # Create your views here.
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from django.template import RequestContext
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from notes.models import Notepad
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 
 @login_required
 def index(request):
 	try:
 		notepad = request.user.notepad
-
-	except ObjectDoesNotExist:
-		notepad = Notepad(body = "", user = request.user)
+	except Notepad.DoesNotExist:
+		notepad = Notepad(body="", user=request.user)
 		notepad.save()
+
+	if request.method == 'GET':
+		return render_to_response('notes/index.html',
+			{
+				"notepad" : notepad
+			},
+			context_instance=RequestContext(request)
+		)
 
 	if request.is_ajax() and request.method == 'POST':
 		notepad.body = request.POST['notepad_text']
 		notepad.save()
 		return HttpResponse("OK")
 
-	return render_to_response('notes/index.html', {"notepad" : notepad }, context_instance=RequestContext(request))
