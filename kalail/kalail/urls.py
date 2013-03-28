8c5cddc7fa64a20e54efcc79ae4b635c19408401
Kalail <kalail@ubuntu.(none)>
2013-03-28 05:39:23 -0500

    Remove keep-alive links.

--- kalail/kalail/urls.py
+++ kalail/kalail/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^blog/', include('blog.urls')),
     url(r'^notes/', include('notes.urls')),
-    url(r'^keep-alive/', include('keepalive.urls')),
+    # url(r'^keep-alive/', include('keepalive.urls')),
     url(r'^browserid/', include('django_browserid.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
